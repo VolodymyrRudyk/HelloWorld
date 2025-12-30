7ab0f5e732f8f9c702c9f491df47c387f7a51ba2
Volodymy Rudyk <rudykvova@gmail.com>
2025-12-30 10:46:31 +0200

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Володя Рудик on 30.12.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
